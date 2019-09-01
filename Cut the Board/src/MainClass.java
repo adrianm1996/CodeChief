f48f6c3f53f25e1fe6f41c3a977ea956fb382af7
admis96 <adrian.misiak.1996@gmail.com>
2019-09-01 15:07:05 +0200

    delete comments

--- Cut the Board/src/MainClass.java
+++ Cut the Board/src/MainClass.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int T;
 		Scanner read = new Scanner(System.in);
 		 T = read.nextInt();
